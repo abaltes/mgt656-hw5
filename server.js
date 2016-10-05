93de22cf971e9b1479b3114f125803c71d8dd9bc
Andrew Baltes <andrew.baltes@yale.edu>
2016-10-05 21:02:38 +0000

    added odd-crocodile

--- server.js
+++ server.js
@@ -5,4 +5,8 @@ app.get('/', function(request, response){
     response.send('Hello MGT-656!');
 });
 
+app.get('/odd-crocodile', function(request, response){
+    response.send('Hello odd-crocodile');
+});
+
 app.listen(process.env.PORT || 4000);
